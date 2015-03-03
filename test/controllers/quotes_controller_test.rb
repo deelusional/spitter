350cbadc6a4a22703b0d62c1dec131f080902794
deelusional <dtapmalai@gmail.com>
2015-03-03 04:01:10 +0000

    Added assert_response

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
    test "quote show page" do
-     assert true
+     quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once')
+     get :show, :id => quote.id
+     assert_response :success
    end
 end
